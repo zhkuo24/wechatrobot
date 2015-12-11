bc25a108be643d13208f774ed8270e5c0994ccd4
zhangkuo <zhkuo1989@gmail.com>
2015-12-11 03:47:44 -0500

    add

--- weixin.py
+++ weixin.py
@@ -1,16 +1,24 @@
 # -*- coding: utf-8 -*-
 import werobot
 import time
+import pytz
+import datetime
 
 robot = werobot.WeRoBot(token='Zhangkuo')
-@robot.handler
-def echo(message):
-    return 'I love You cuicui!'
+#@robot.handler
+#def echo(message):
+#    return 'I love You cuicui!'
+
+#@robot.text
+#def echo(message):
+#    return message.content + '\n' + 'I Love You CuiCui' + "张阔\n" + time.strftime("%Y-%m-%d-%H:%M:%S %Z",time.localtime(time.time()))
+
 
 @robot.text
 def echo(message):
-    return message.content + '\n' + 'I Love You CuiCui' + "张阔\n" + time.strftime("%Y-%m-%d-%H:%M:%S %Z",time.localtime(time.time()))
-@robot.filter("a")
-def a():
-    return "正文为 a "
+	#获取时间信息
+	tz = pytz.timezone('Asia/Shanghai')
+	time_current = datetime.datetime.now(tz).strftime('%Y-%m-%d %H:%M:%S')
+	return message.content + '\n' + 'I Love You CuiCui 来自WALL-E\n' + 'Time: ' + time_current
+
 robot.run()
